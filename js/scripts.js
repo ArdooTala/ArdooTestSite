910857fb4527e55202160efaba4abc0d5ed1b92f
Ardoo <ardeshir.talaei@iaac.net>
2019-08-20 22:41:13 +0430

    add Skills motion

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,18 @@ function getDescription(doc){
   var des = null;
   var controls = doc.querySelectorAll('p');
   for (var i = 0; i < controls.length; i++) {
-    if (controls[i].className == "description") {
+    if (controls[i].className.includes("description")) {
+      return controls[i];
+    }
+  }
+}
+
+function getUL(doc){
+  var note = null;
+  var des = null;
+  var controls = doc.querySelectorAll('ul');
+  for (var i = 0; i < controls.length; i++) {
+    if (controls[i].className.includes("cats")) {
       return controls[i];
     }
   }
@@ -12,25 +23,37 @@ function getDescription(doc){
 function changeState(elc){
   var desc = getDescription(elc);
   if (desc.style.display == 'block') {
-    delaborate(elc);
+    delaborate(elc, getDescription(elc));
   }
   else {
-    elaborate(elc);
+    elaborate(elc, getDescription(elc));
   }
 }
 
-function elaborate(el){
+function changeUL(elc){
+  var desc = getUL(elc);
+  if (desc.style.display == 'block') {
+    delaborate(elc, getUL(elc));
+  }
+  else {
+    elaborate(elc, getUL(elc));
+  }
+}
+
+function elaborate(el, desc){
   el.style.border = '1px solid black';
   el.style.borderRadius = "1em";
-  var desc = getDescription(el);
   desc.style.display='block';
   //desc.style.height = "0px";
   myMove(desc);
   function myMove(elem) {
     var h = 0;
+    elem.style.height = 'auto';
+    var hd = elem.offsetHeight;
+    elem.style.height = 0;
     var id = setInterval(frame, 1);
     function frame() {
-      if (h >= 50) {
+      if (h >= hd) {
         clearInterval(id);
       } else {
         h += 3;
@@ -41,16 +64,15 @@ function elaborate(el){
 }
 
 
-function delaborate(el){
-  var desc = getDescription(el);
+function delaborate(el, desc){
   myMove(desc);
   function myMove(elem) {
-    var h = 50;
+    var h = elem.offsetHeight;
     var id = setInterval(frame, 1);
     function frame() {
       if (h <= 0) {
-          desc.style.display='none';
-          el.style.border = 'none';
+        desc.style.display='none';
+        el.style.border = 'none';
         clearInterval(id);
       } else {
         h -= 3;
