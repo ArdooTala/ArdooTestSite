2cb92476fed6a2b7e36fcaf049eef72567dbf415
Ardoo <ardeshir.talaei@iaac.net>
2019-08-22 18:52:35 +0430

    add Motion Graphics

--- js/HomeCanvas.js
+++ js/HomeCanvas.js
@@ -2,12 +2,14 @@ var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
 class LinkNode {
-  constructor(x, y) {
+  constructor(x, y, tag, size) {
     this.x = x;
     this.y = y;
     this.heading = {vx: Math.random(), vy:Math.random(),};
     this.heading = this.unitize(this.heading.vx, this.heading.vy);
     this.neighbor;
+    this.tag = tag;
+    this.size = size;
   }
 
   distanceTo(x, y){
@@ -60,10 +62,35 @@ class LinkNode {
   }
 }
 
+var mouseX, mouseY;
+document.onmousemove = getMousePos;
+
+function getMousePos(evt) {
+    var rect = canvas.getBoundingClientRect();
+    mouseX = (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+    mouseY = (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
+    // ctx.strokeStyle = "red";
+    // ctx.beginPath();
+    // ctx.arc(mouseX, mouseY, 60, 0, 2*Math.PI);
+    // ctx.stroke();
+    // ctx.strokeStyle = "black";
+}
+
+function remapRange(val, sMin, sMax, tMin, tMax) {
+  val -= sMin;
+  var sRange = sMax - sMin;
+  var tRange = tMax - tMin;
+  return Math.max(Math.min((val / sRange) * tRange + tMin, tMin), tMax);
+}
+
+var tags = ["LinkedIn", "Instagram", "GitHub", "Facebook", "Projects", "Skills",
+ "Contact", "CV", "Publications", "Project1", "Project2", "Project3"];
+var sizes = [3, 3, 3, 3, 10, 8, 5, 6, 5, 3, 7, 5]
 var agents = [];
-for (var i=0; i < 20; i++){
-  agents[i] = new LinkNode(Math.random()*50+canvas.width/2-25,
-                           Math.random()*50+canvas.height/2-25);
+for (var i=0; i < 12; i++){
+  agents[i] = new LinkNode(Math.random()*1800+canvas.width/2-900,
+                           Math.random()*10+canvas.height/2-5,
+                           tags[i], sizes[i]);
 }
 
 ctx.beginPath();
@@ -73,19 +100,41 @@ for (var i=0; i < agents.length; i++){
 ctx.stroke();
 
 var maxDisp = 100;
-var id = setInterval(frame, 4);
+var id = setInterval(frame, 2);
 function frame() {
+  ctx.fillStyle = "white";
+  ctx.globalAlpha = 0.1;
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+  ctx.globalAlpha = 1;
+
   for (var i=0; i < agents.length; i++){
     agents[i].updateHeading(agents);
   }
+
+  var mouseClosestAgent = agents[0];
+  var mouseC = 2000;
   for (var i=0; i < agents.length; i++){
-    agents[i].updatePos(Math.min(50, 500/maxDisp));
+    var mD = agents[i].distanceTo(mouseX, mouseY);
+    if (mD < mouseC) {
+      mouseC = mD;
+      mouseClosestAgent = agents[i];
+    }
+  }
+  mouseClosestAgent.heading = mouseClosestAgent.unitize(mouseClosestAgent.x-mouseX, mouseClosestAgent.y-mouseY);
+  ctx.beginPath();
+  ctx.lineWidth = 1;
+  ctx.arc(mouseClosestAgent.x, mouseClosestAgent.y, 40, 0, 2*Math.PI);
+  ctx.stroke();
+
+  for (var i=0; i < agents.length; i++){
+    if (agents[i] == mouseClosestAgent) {
+      mouseClosestAgent.updatePos(Math.min(2, mouseClosestAgent.distanceTo(mouseX, mouseY)/ (canvas.width/20)));
+    }
+    else {
+      agents[i].updatePos(Math.max(0.1, Math.min(10, 500/maxDisp)));
+    }
   }
 
-  ctx.fillStyle = "white";
-  ctx.globalAlpha = 0.01;
-  ctx.fillRect(0,0,canvas.width, canvas.height);
-  ctx.globalAlpha = 1;
 
   // ctx.beginPath();
   // ctx.moveTo(agents[agents.length-1].x, agents[agents.length-1].y);
@@ -94,18 +143,26 @@ function frame() {
   // }
   // ctx.stroke();
 
-  ctx.fillStyle = "black";
   for (var i=0; i < agents.length; i++){
     ctx.beginPath();
-    ctx.arc(agents[i].x, agents[i].y, 5, 0, 2*Math.PI);
-    ctx.fill();
-
-    ctx.beginPath();
+    ctx.lineWidth = remapRange(agents[i].distanceTo(agents[i].neighbor.x, agents[i].neighbor.y), canvas.width/20, canvas.width/5, 30, 1);
     ctx.moveTo(agents[i].x, agents[i].y);
     ctx.lineTo(agents[i].neighbor.x, agents[i].neighbor.y);
     ctx.stroke();
+
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc(agents[i].x, agents[i].y, 30+0*agents[i].size, 0, 2*Math.PI);
+    ctx.fill();
+    // ctx.fillStyle = "white";
+    // ctx.beginPath();
+    // ctx.arc(agents[i].x, agents[i].y, 40+0*agents[i].size, 0, 2*Math.PI);
+    // ctx.fill();
+
+    // ctx.font = "bold " + agents[i].size*5 + "px roboto";
+    // ctx.fillText(agents[i].tag, agents[i].x, agents[i].y+10, 1000);
   }
 
   maxDisp++;
-  if (maxDisp > 100000) clearInterval(id);
+  if (maxDisp > 10000) clearInterval(id);
 }
