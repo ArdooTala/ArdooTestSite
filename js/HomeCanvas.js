de57a800fe07c8e40541229a7e164d53d6327251
Ardoo <ardeshir.talaei@iaac.net>
2019-08-23 18:16:21 +0430

    edit Motion Graphics

--- js/HomeCanvas.js
+++ js/HomeCanvas.js
@@ -10,6 +10,7 @@ class LinkNode {
     this.neighbor;
     this.tag = tag;
     this.size = size;
+    this.clearance = 0;
   }
 
   distanceTo(x, y){
@@ -29,9 +30,8 @@ class LinkNode {
 
   updateHeading(others){
     // Min distance to edge.
-    var nx, ny;
-    nx = Math.min(this.x+200, canvas.width+200-this.x)*(this.x-canvas.width/2)/Math.abs(this.x-canvas.width/2);
-    ny = Math.min(this.y+200, canvas.height+200-this.y)*(this.y-canvas.height/2)/Math.abs(this.y-canvas.height/2);
+    var nx = Math.min(this.x+200, canvas.width+200-this.x)*(this.x-canvas.width/2)/Math.abs(this.x-canvas.width/2);
+    var ny = Math.min(this.y+200, canvas.height+200-this.y)*(this.y-canvas.height/2)/Math.abs(this.y-canvas.height/2);
     var d = Math.min(Math.abs(nx), Math.abs(ny));
 
     if (Math.abs(nx) > Math.abs(ny)) nx = 0;
@@ -46,16 +46,17 @@ class LinkNode {
         ny = others[j].y - this.y;;
       }
     }
+    this.clearance = d;
     var nHeading = this.unitize(nx, ny);
-    this.heading.vx += .1*nHeading.vx;
-    this.heading.vy += .1*nHeading.vy;
+    this.heading.vx += 0.1*nHeading.vx;
+    this.heading.vy += 0.1*nHeading.vy;
     this.heading = this.unitize(this.heading.vx, this.heading.vy);
   }
 
   updatePos(percent){
     if (this.heading.vx != 0 | this.heading.vy != 0) {
-      this.x -= this.heading.vx*percent;
-      this.y -= this.heading.vy*percent;
+      this.x += -this.heading.vx*percent;
+      this.y += -this.heading.vy*percent;
       this.x = Math.max(Math.min(this.x, canvas.width-10), 10);
       this.y = Math.max(Math.min(this.y, canvas.height-10), 10);
     }
@@ -88,8 +89,8 @@ var tags = ["LinkedIn", "Instagram", "GitHub", "Facebook", "Projects", "Skills",
 var sizes = [3, 3, 3, 3, 10, 8, 5, 6, 5, 3, 7, 5]
 var agents = [];
 for (var i=0; i < 12; i++){
-  agents[i] = new LinkNode(Math.random()*1800+canvas.width/2-900,
-                           Math.random()*10+canvas.height/2-5,
+  agents[i] = new LinkNode(Math.random()*1500+canvas.width/2-750,
+                           Math.random()*20+canvas.height/2-10,
                            tags[i], sizes[i]);
 }
 
@@ -99,7 +100,6 @@ for (var i=0; i < agents.length; i++){
 }
 ctx.stroke();
 
-var maxDisp = 100;
 var id = setInterval(frame, 2);
 function frame() {
   ctx.fillStyle = "white";
@@ -111,8 +111,8 @@ function frame() {
     agents[i].updateHeading(agents);
   }
 
-  var mouseClosestAgent = agents[0];
-  var mouseC = 2000;
+  var mouseClosestAgent; // = agents[0];
+  var mouseC = canvas.width;
   for (var i=0; i < agents.length; i++){
     var mD = agents[i].distanceTo(mouseX, mouseY);
     if (mD < mouseC) {
@@ -120,19 +120,23 @@ function frame() {
       mouseClosestAgent = agents[i];
     }
   }
-  mouseClosestAgent.heading = mouseClosestAgent.unitize(mouseClosestAgent.x-mouseX, mouseClosestAgent.y-mouseY);
-  ctx.beginPath();
-  ctx.lineWidth = 1;
-  ctx.arc(mouseClosestAgent.x, mouseClosestAgent.y, 40, 0, 2*Math.PI);
-  ctx.stroke();
+  if (mouseClosestAgent) {
+    mouseClosestAgent.heading = mouseClosestAgent.unitize(mouseClosestAgent.x-mouseX, mouseClosestAgent.y-mouseY);
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.arc(mouseClosestAgent.x, mouseClosestAgent.y, 40, 0, 2*Math.PI);
+    ctx.stroke();
+  }
 
   for (var i=0; i < agents.length; i++){
     if (agents[i] == mouseClosestAgent) {
       mouseClosestAgent.updatePos(Math.min(2, mouseClosestAgent.distanceTo(mouseX, mouseY)/ (canvas.width/20)));
     }
     else {
-      agents[i].updatePos(remapRange(agents[i].distanceTo(agents[i].neighbor.x, agents[i].neighbor.y), canvas.width/20, canvas.width/10, 10, .3));
+      var v = remapRange(agents[i].clearance, canvas.width/20, canvas.width/10, 10, 0.2);
+      agents[i].updatePos(v);
     }
+    console.log(agents[i].x + ", " + agents[i].y);
   }
 
   // Math.min(2, agents[i].distanceTo(agents[i].neighbor.x, agents[i].neighbor.y) / (canvas.width/20))
@@ -145,7 +149,7 @@ function frame() {
 
   for (var i=0; i < agents.length; i++){
     ctx.beginPath();
-    ctx.lineWidth = remapRange(agents[i].distanceTo(agents[i].neighbor.x, agents[i].neighbor.y), canvas.width/20, canvas.width/5, 30, 1);
+    ctx.lineWidth = remapRange(agents[i].clearance, canvas.width/20, canvas.width/10, 30, 1);
     ctx.moveTo(agents[i].x, agents[i].y);
     ctx.lineTo(agents[i].neighbor.x, agents[i].neighbor.y);
     ctx.stroke();
@@ -158,11 +162,9 @@ function frame() {
     // ctx.beginPath();
     // ctx.arc(agents[i].x, agents[i].y, 40+0*agents[i].size, 0, 2*Math.PI);
     // ctx.fill();
-
     // ctx.font = "bold " + agents[i].size*5 + "px roboto";
     // ctx.fillText(agents[i].tag, agents[i].x, agents[i].y+10, 1000);
   }
-
-  maxDisp++;
-  // if (maxDisp > 10000) clearInterval(id);
+  console.log(agents.length);
+  // clearInterval(id);
 }
