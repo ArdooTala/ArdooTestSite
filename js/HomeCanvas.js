a8b6ed6a42a29345289d6b7cf87f225560d3c047
Ardeshir Talaei <Ardeshir.talaei@iaac.net>
2019-08-24 22:21:56 +0430

    Decrease Banner Agents Size

--- js/HomeCanvas.js
+++ js/HomeCanvas.js
@@ -144,7 +144,7 @@ function shrinkCanvas(evt) {
       activeAgents = 5;
       agents.length = 5;
       for (var j=0; j<activeAgents;j++){
-        agents[j].size /= 3;
+        agents[j].size = (sizes[i]/300)*longerEdge;
       }
       win.style.height = (.88*height) + "px";
       restDist = canvasTargetHeight/11;
